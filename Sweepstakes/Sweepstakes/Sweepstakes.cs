59dc61d15022660ca4b2774eca20567944e7ea36
Clint <crob2099@gmail.com>
2020-11-02 15:13:33 -0500

    Created Sweepstakes.cs and initial variables and skeletons for methods in Sweepstakes.cs

---
+++ Sweepstakes/Sweepstakes/Sweepstakes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Sweepstakes
+    {
+
+        private Dictionary<int, Contestant> contestants;
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        public Sweepstakes(string name)
+        {
+            Name = name;
+        }   
+
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+
+        public Contestant PickWinner()
+        {
+
+        }
+
+        public void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+
+
+
+
+    }
+}
--- Sweepstakes/Sweepstakes/UserInterface.cs
+++ Sweepstakes/Sweepstakes/UserInterface.cs
@@ -46,7 +46,8 @@ namespace Sweepstakes
             return registrationNumber;
         }
 
-      
+
+
 
 
     }
