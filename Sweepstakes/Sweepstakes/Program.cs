e7467fb568580b2ea0b3c6d89dcd2ee58a2a5b21
Clint <crob2099@gmail.com>
2020-11-03 11:11:54 -0500

    Created MarketingFirm.cs and initial method skeletons. Also, added comments in Program.cs as I'm testing out different parts of the project.

--- Sweepstakes/Sweepstakes/Program.cs
+++ Sweepstakes/Sweepstakes/Program.cs
@@ -10,15 +10,22 @@ namespace Sweepstakes
     {
         static void Main(string[] args)
         {
+            //Create contestants and sweepstakes
             Contestant contestant1 = new Contestant();
             Contestant contestant2 = new Contestant();
             Sweepstakes firstSweepstakes = new Sweepstakes("Monopoly");
 
+            //register contestants and print info
             firstSweepstakes.RegisterContestant(contestant1);
             firstSweepstakes.RegisterContestant(contestant2);
-
             firstSweepstakes.PrintContestantInfo(contestant1);
             firstSweepstakes.PrintContestantInfo(contestant2);
+
+            //Sweepstakes manager
+            SweepstakesQueueManager queue1 = new SweepstakesQueueManager();
+            queue1.InsertSweepstakes(firstSweepstakes);
+            Sweepstakes contestThisTime = queue1.GetSweepstakes();
+            Console.WriteLine($"The contest that was chosen this time was {contestThisTime.Name}!");
 
             Console.ReadLine();
         }
