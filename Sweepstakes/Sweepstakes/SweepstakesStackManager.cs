08c4f8c37e8bfcaa96173418ab4a31f1412f5c50
Clint <crob2099@gmail.com>
2020-11-03 09:30:39 -0500

    Created SweepstakesQueueManager.cs and SweepstakesStackManager.cs

---
+++ Sweepstakes/Sweepstakes/SweepstakesStackManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class SweepstakesStackManager
+    {
+        private Stack<Sweepstakes> stack;
+
+
+
+
+
+    }
+}
